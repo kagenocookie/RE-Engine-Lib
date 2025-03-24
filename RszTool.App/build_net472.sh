cea70d854ea61e09b785f17e2f47cc2005ce95a4
czastack <2464851375@qq.com>
2025-03-25 00:04:03 +0800

    feat: update version v0.3.4

--- RszTool.App/build_net472.sh
+++ RszTool.App/build_net472.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+version=`grep -oP '<AssemblyVersion>\K(\d+\.\d+\.\d+)' RszTool.App.csproj`
 C:/Users/An/source/repos/ChenStackTrainer/Packer/pack.sh --ms-build
 cp -r bin/Release/net472/Data bin/Release/net472/zh-CN publish
 cd publish
-version=0.3.3
 rm *.zip
 mv RszTool.AppTrainer-$version-chenstack-net472.exe RszTool.App-$version-net472.exe
 mv RszTool.AppTrainer-$version-chenstack-net8.exe RszTool.App-$version-net8.exe

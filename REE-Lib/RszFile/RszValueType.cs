2e38c096cb287f7fd86ec464885f68184e9d8277
shadowcookie <45361488+kagenocookie@users.noreply.github.com>
2026-04-19 01:23:59 +0200

    Fix inequality operator for via.Position

--- REE-Lib/RszFile/RszValueType.cs
+++ REE-Lib/RszFile/RszValueType.cs
@@ -755,7 +755,7 @@ namespace ReeLib.via
 
         public bool Equals(Position other) => other == this;
         public static bool operator==(Position p1, Position p2) => p1.x == p2.x && p1.y == p2.y && p1.z == p2.z;
-        public static bool operator!=(Position p1, Position p2) => p1.x == p2.x && p1.y == p2.y && p1.z == p2.z;
+        public static bool operator!=(Position p1, Position p2) => p1.x != p2.x || p1.y != p2.y || p1.z != p2.z;
 
         public readonly override string ToString() => $"Position({x}, {y}, {z})";
 
